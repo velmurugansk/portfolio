598f5a17e7d3970959d33a7f3badea494102245c
velmurugansk <velmurugan.k@zedbee.in>
2023-05-12 18:34:13 +0530

    skills

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,15 +1,59 @@
 import React from 'react'
+import {AiFillHtml5} from "react-icons/ai";
+import {SiCss3,SiTailwindcss, SiBootstrap, SiJquery,SiReact} from "react-icons/si";
+import {IoLogoJavascript} from "react-icons/io";
 
 const Skills = () => {
   return (
-    <div className='w-full lg:h-screen p-2 px-3'>
-        <div className="flex mx-auto flex-col justify-center h-full">
-            <p className='uppercase py-3 text-[#5651e5] tracking-widest'>Skills</p>
-            <h2 className="py-3">What I can do</h2>
-            <div className='grid md:grid-cols-4 lg:grid-cols-2'>
-                
+    <div className='w-full mx-auto md:h-screen p-2 px-24'>
+      <div className="flex mx-auto flex-col justify-center h-full">
+        <p className='uppercase py-3 text-[#5651e5] tracking-widest'>Skills</p>
+        <h2 className="py-3">What I can do</h2>
+        <div className='grid md:grid-cols-4 lg:grid-cols-2 gap-4'>
+          <div className="lg:col-span-1 md:col-span-2 py-2 px-4 bg-white shadow-lg">
+            <div className="flex justify-between items-center">
+              <p>HTML</p>
+              <AiFillHtml5 />
             </div>
+          </div>
+          <div className="lg:col-span-1 md:col-span-2 bg-white py-2 px-4 shadow-lg">
+            <div className="flex justify-between items-center">
+              <p>CSS</p>
+              <SiCss3 />
+            </div>
+          </div>
+          <div className="lg:col-span-1 md:col-span-2 py-2 px-4 bg-white shadow-lg">
+            <div className="flex justify-between items-center">
+              <p>Tailwind</p>
+              <SiTailwindcss />
+            </div>
+          </div>
+          <div className="lg:col-span-1 md:col-span-2 bg-white py-2 px-4 shadow-lg">
+            <div className="flex justify-between items-center">
+              <p>Bootstrap 4</p>
+              <SiBootstrap />
+            </div>
+          </div>
+          <div className="lg:col-span-1 md:col-span-2 py-2 px-4 bg-white shadow-lg">
+            <div className="flex justify-between items-center">
+              <p>Javascript</p>
+              <IoLogoJavascript />
+            </div>
+          </div>
+          <div className="lg:col-span-1 md:col-span-2 bg-white py-2 px-4 shadow-lg">
+            <div className="flex justify-between items-center">
+              <p>JQuery</p>
+              <SiJquery />
+            </div>
+          </div>
+          <div className="lg:col-span-1 md:col-span-2 bg-white py-2 px-4 shadow-lg">
+            <div className="flex justify-between items-center">
+              <p>React JS</p>
+              <SiReact />
+            </div>
+          </div>
         </div>
+      </div>
     </div>
   )
 }
