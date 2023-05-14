fb9b0656ddf8792f66f4195f4d7427b8fb5834c5
velmurugansk <velmurugan.k@zedbee.in>
2023-05-14 17:38:01 +0530

    projects

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -9,47 +9,47 @@ const Skills = () => {
       <div className="flex mx-auto flex-col justify-center h-full">
         <p className='uppercase py-3 text-[#5651e5] tracking-widest'>Skills</p>
         <h2 className="py-3">What I can do</h2>
-        <div className='grid md:grid-cols-4 lg:grid-cols-2 gap-4'>
+        <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-4'>
           <div className="lg:col-span-1 md:col-span-2 py-2 px-4 bg-white shadow-lg">
             <div className="flex justify-between items-center">
               <p>HTML</p>
-              <AiFillHtml5 />
+              <AiFillHtml5 size={24} />
             </div>
           </div>
           <div className="lg:col-span-1 md:col-span-2 bg-white py-2 px-4 shadow-lg">
             <div className="flex justify-between items-center">
               <p>CSS</p>
-              <SiCss3 />
+              <SiCss3 size={24} />
             </div>
           </div>
           <div className="lg:col-span-1 md:col-span-2 py-2 px-4 bg-white shadow-lg">
             <div className="flex justify-between items-center">
               <p>Tailwind</p>
-              <SiTailwindcss />
+              <SiTailwindcss size={24} />
             </div>
           </div>
           <div className="lg:col-span-1 md:col-span-2 bg-white py-2 px-4 shadow-lg">
             <div className="flex justify-between items-center">
               <p>Bootstrap 4</p>
-              <SiBootstrap />
+              <SiBootstrap size={24} />
             </div>
           </div>
           <div className="lg:col-span-1 md:col-span-2 py-2 px-4 bg-white shadow-lg">
             <div className="flex justify-between items-center">
               <p>Javascript</p>
-              <IoLogoJavascript />
+              <IoLogoJavascript size={24} />
             </div>
           </div>
           <div className="lg:col-span-1 md:col-span-2 bg-white py-2 px-4 shadow-lg">
             <div className="flex justify-between items-center">
               <p>JQuery</p>
-              <SiJquery />
+              <SiJquery size={24} />
             </div>
           </div>
           <div className="lg:col-span-1 md:col-span-2 bg-white py-2 px-4 shadow-lg">
             <div className="flex justify-between items-center">
               <p>React JS</p>
-              <SiReact />
+              <SiReact size={24} />
             </div>
           </div>
         </div>
