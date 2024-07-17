6beb4e5cc7a400f12ab257fe3a0b2113a7e4c61c
skvelmurugan <velmurugan.k@zedbee.in>
2024-07-17 21:13:14 +0530

    header nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+const Titles = [{name : "Home"},{name : "Skills"},{name : "Contact"}]
 
 const Header = () => {
+
   return (
-    <div className='headnav 2xl px-4 py-2'>
-      Header
+    <div className='2xl px-4 py-2 flex justify-end'>
+      <nav>
+      <ul className='flex'>
+        {Titles.map(elem => {          
+          return (<li className='px-3 cursor-pointer' key={elem.name}>{elem.name}</li>)
+        })}
+      </ul>
+      </nav>
     </div>
   )
 }
