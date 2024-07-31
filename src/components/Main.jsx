3aeb8209f2263ef2d5491498a26db9bdaf493a2d
skvelmurugan <velmurugan.k@zedbee.in>
2024-07-31 23:12:36 +0530

    resume download

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import hero from './../assets/images/web-dev.svg'
+import hero from './../assets/images/web-dev.svg';
+import resume from '../assets/resume.pdf';
 
 const Main = () => {
   return (
@@ -9,7 +10,8 @@ const Main = () => {
           <h1 className='text-3xl md:text-4xl name-txt font-bold text-white'>Hello, I'm S.K.VELMURUGAN</h1>
           <h2 className='font-bold text-2xl md:text-3xl py-3 animatedtext text-[#8644A2]'>Frontend Developer.</h2>
           <p className='py-4 text-[#8892b0]'>I'm focused on building responsive frontend applications.</p>
-          <div><button className='text-white px-6 py-2 border-2 hover:bg-[#8644A2] hover:border-[#8644A2]'>Download CV</button></div>
+          <div><a href={resume} download="resume"><button className='text-white px-6 py-2 border-2 hover:bg-[#8644A2] hover:border-[#8644A2]'>
+            Download CV</button></a></div>
         </div>
         <div className='mx-auto my-1'>
           <img src={hero} className='object-fill h-60 w-60 md:h-80 md:w-80' alt="devloperimg" />
